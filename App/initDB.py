717ee90661d0c104c879008493da99f82235926b
Mark-Khelawan <keonkhelawan@gmail.com>
2022-03-28 19:42:50 +0000

    1

--- App/initDB.py
+++ App/initDB.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-file = pd.read_excel('./App/job_course.xlsx', index_col=0, header=2, usecols="A:F")
+file = pd.read_excel('./App/job_course.xlsx', index_col=1, header=2, usecols="A:F")
 
 print(file)
